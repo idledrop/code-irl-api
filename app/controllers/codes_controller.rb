1750a9659563177152b2a8973ec198f9cdc8036a
Joshua Livesay <joshualivesay@gmail.com>
2016-06-18 16:43:52 -0400

    did some stuff

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -4,6 +4,7 @@ class CodesController < ApplicationController
   # GET /codes
   def index
     if params[:tags].present?
+
       tags = params[:tags].split(',')
       @codetags = CodeTag.where(:tag_id => tags)
       codes = @codetags.map do |codetag|
@@ -12,8 +13,11 @@ class CodesController < ApplicationController
       @codes = Code.where(:id => codes)
     elsif params[:tag_id].present?
       @codes = Tag.find(params[:tag_id]).codes
+
+    else
+      @codes = Code.all
     end
-    @codes = Code.all
+
 
     render json: @codes
   end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ApplicationRecord
   has_many :code_tags
-  has_many :tags, :through => :code_tags
-  validates_presence_of :name
+  has_many :codes, :through => :code_tags
+  validate :name, :presence => true
 
 end
